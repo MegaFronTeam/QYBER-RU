23fe2de86e8b930367b25ec04e81db183b3df1dd
jpaberzs <janis.paberzs18@gmail.com>
2024-04-17 05:26:40 +0300

    done header and footer

---
+++ components/Contacts.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="contacts">
+    <li>
+      <a href="#" class="contacts__link" target="_blank">
+        <svg-icon name="youtube" />
+      </a>
+    </li>
+    <li>
+      <a href="#" class="contacts__link" target="_blank">
+        <svg-icon name="twitch" />
+      </a>
+    </li>
+    <li>
+      <a href="#" class="contacts__link" target="_blank">
+        <svg-icon name="vk.svg" />
+      </a>
+    </li>
+    <li>
+      <a href="#" class="contacts__link" target="_blank">
+        <svg-icon name="telegram" />
+      </a>
+    </li>
+  </ul>
+</template>
