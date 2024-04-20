2e77981e1fe23f69c99e2db85d7036c8520518bf
jpaberzs <janis.paberzs18@gmail.com>
2024-04-20 05:47:03 +0300

    edit all news apge

---
+++ components/Subscribe.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="sSubscribe">
+    <div class="container">
+      <div class="sSubscribe__wrap bg-wrap">
+        <NuxtImg src="/img/sSubscribe-bg-1.jpg" alt="bg" class="picture-bg sSubscribe__bg" />
+        <div class="row">
+          <div class="sSubscribe__col-img col">
+            <NuxtImg src="/img/sSubscribe-img-1.png" alt="Juggernaut" class="sSubscribe__img" />
+          </div>
+          <div class="sSubscribe__col col">
+            <div class="h1">Подпишитесь на актуальные новости в нашем телеграм канале</div>
+            <p>
+              Все новости турнира, интервью, видео, аналитика и общение – все это в телеграм канале
+            </p>
+            <Button severity="secondary">
+              <span> Перейти в Telegram</span>
+              <svg-icon name="telegram.svg" />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
