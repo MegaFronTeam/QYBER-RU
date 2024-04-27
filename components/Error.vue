143f9c4ed943e7f2cca60728b4ce48ceed82c23f
jpaberzs <janis.paberzs18@gmail.com>
2024-04-27 03:25:39 +0300

    feat: add 404 page

---
+++ components/Error.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="sError">
+    <div class="container">
+      <span>Добро пожаловать на страницу {{ errorMsg }}!</span>
+      <h1>К сожалению запрашиваемая страница не найдена</h1>
+      <p>
+        Скорее всего, это случилось по одной из следующих причин: страница переехала, страницы
+        больше нет или возможно, вы допустили небольшую опечатку при вводе адреса.
+      </p>
+      <NuxtLink to="/">
+        <Button class="btn-lg"> Вернуться на главную </Button>
+      </NuxtLink>
+    </div>
+  </section>
+</template>
+
+<script setup>
+const defineProps = defineProps({
+  errorMsg: {
+    type: String,
+    required: false,
+  },
+});
+const { errorMsg } = defineProps;
+</script>
---
+++ error.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app';
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+</script>
+
+<template>
+  <div class="main-wrapper">
+    <AppHeader />
+    <main>
+      <Error :errorMsg="error?.statusCode" />
+      <!-- <h1>{{ error.statusCode }}</h1>
+      <NuxtLink to="/">Go back home</NuxtLink> -->
+      <Subscribe />
+    </main>
+    <AppFooter />
+  </div>
+</template>
+
+<style>
+@import './assets/scss/normalize.scss';
+@import './assets/scss/main.scss';
+</style>
