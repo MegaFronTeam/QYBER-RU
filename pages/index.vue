38a9bed63bb94c21e5f887ee6a5e80555a81cdcb
jpaberzs <janis.paberzs18@gmail.com>
2024-05-18 06:05:05 +0300

    edit index page

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,23 @@
       В разработке...
     </MainContentBlock>
     <MainContentBlock :sectionTitle="sectionTitleNewsProps" :footerLink="footerLinkNewsProps">
-      Need content for 4 last cards
+      <NewsCard
+        v-for="item of data.related"
+        :newsData="{
+          id: item.ID,
+          title: item.post_title,
+          date: item.post_date,
+          excerpt: item.post_excerpt,
+          thumbnail: item.post_thumbnail,
+        }"
+        :key="item.id"
+      />
     </MainContentBlock>
   </div>
 </template>
 
 <script setup>
+const { data } = await useFetch(`https://qyber.ru/wordpress/wp-json/wp/v2/posts/1`);
 const sectionTitleTournamentsProps = {
   title: 'Ближайшие турниры',
   text: 'Ты лидер команды? Подай заявку на участие в ближайшем турнире!',
