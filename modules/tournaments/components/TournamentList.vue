26819a80be1efada64ac6c34240975217ec48ea2
jpaberzs <janis.paberzs18@gmail.com>
2024-06-12 11:36:04 +0300

    feat: implement using url querry params

--- modules/tournaments/components/TournamentList.vue
+++ modules/tournaments/components/TournamentList.vue
@@ -3,27 +3,26 @@
     <div class="container">
       <div class="template template--header">
         <div class="btn-wrap template-wrap">
-          <Button @click="active = 0" :class="active === 0 ? 'active' : ''"> Предстоящие </Button>
+          <Button @click="clickOnTab(0)" :class="active === 0 ? 'active' : ''">
+            Предстоящие
+          </Button>
           <!-- v-if="teamsStore.myTeamsCount > 0" -->
-          <Button @click="active = 1" :class="active === 1 ? 'active' : ''">
+          <Button @click="clickOnTab(1)" :class="active === 1 ? 'active' : ''">
             Текущие
             <Badge v-if="currentTournaments.length > 0" :value="currentTournaments.length" />
           </Button>
-          <Button @click="active = 2" :class="active === 2 ? 'active' : ''"> Завершенные </Button>
+          <Button @click="clickOnTab(2)" :class="active === 2 ? 'active' : ''">
+            Завершенные
+          </Button>
         </div>
       </div>
       <div class="template">
         <div class="sTournamentList__info">
-          <span>
-            {{
-              active === 0
-                ? upcomingTournaments.length
-                : active === 1
-                ? currentTournaments.length
-                : endedTournaments.length
-            }}
-          </span>
-          <InputSwitch v-model="checked" class="sTournamentList__changeVision ms-auto" />
+          <InputSwitch
+            v-model="checked"
+            @click="clickOnStandings(checked)"
+            class="sTournamentList__changeVision ms-auto"
+          />
         </div>
         <TabView v-model:activeIndex="active">
           <TabPanel>
@@ -185,14 +184,33 @@
   </section>
 </template>
 <script setup>
+import { useRoute, useRouter } from 'vue-router';
 import { useTournamentStore } from '@/store/TournamentStore';
 
+const route = useRoute();
+const router = useRouter();
 const tournamentStore = useTournamentStore();
 const { upcomingTournaments, currentTournaments, endedTournaments } = storeToRefs(tournamentStore);
-const active = ref(1);
-const checked = ref(false);
+const querryList = ref({
+  tab: route.query.tab ? Number(route.query.tab) : 1,
+  grid: route.query.grid ? Boolean(route.query.grid) : false,
+});
+const active = ref(querryList.value.tab);
+const checked = ref(querryList.value.grid);
 
-console.log(upcomingTournaments.value);
+const clickOnTab = (id) => {
+  // console.log(route);
+  active.value = id;
+  router.push({
+    query: { ...route.query, tab: id },
+  });
+};
+
+const clickOnStandings = (state) => {
+  router.push({
+    query: { ...route.query, grid: state },
+  });
+};
 
 tournamentStore.getAll().then(() => {
   tournamentStore.sortTournamnts();
