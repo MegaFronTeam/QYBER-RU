8431704a375552406c9dee0df686acf465de7c0d
PAVEL KHALUNIN <wol1414@gmail.com>
2024-07-09 12:29:26 +0200

    fix: if for  show active buttons

--- components/share/FilterBlock.vue
+++ components/share/FilterBlock.vue
@@ -32,79 +32,79 @@
 </template>
 
 <script setup>
-import { useFilterStore } from '@/store/FilterStore';
-const router = useRouter();
-const route = useRoute();
+  import { useFilterStore } from '@/store/FilterStore';
+  const router = useRouter();
+  const route = useRoute();
 
-const props = defineProps({
-  fetchMethod: {
-    type: Function,
-    required: true,
-  },
-  firstAction: {
-    type: Boolean,
-    default: true,
-  },
-});
-const { fetchMethod, firstAction } = props;
+  const props = defineProps({
+    fetchMethod: {
+      type: Function,
+      required: true,
+    },
+    firstAction: {
+      type: Boolean,
+      default: true,
+    },
+  });
+  const { fetchMethod, firstAction } = props;
 
-const filterStore = useFilterStore();
+  const filterStore = useFilterStore();
 
-await filterStore.fetchLeagues();
-await filterStore.fetchDiscipline();
+  await filterStore.fetchLeagues();
+  await filterStore.fetchDiscipline();
 
-const { leagues, discipline } = storeToRefs(filterStore);
+  const { leagues, discipline } = storeToRefs(filterStore);
 
-const filter = ref({
-  leagues: 0,
-  discipline: 0,
-});
+  const filter = ref({
+    leagues: 0,
+    discipline: 0,
+  });
 
-const changeFilter = (key, value, notFetch) => {
-  filter.value[key] = +value;
+  const changeFilter = (key, value, notFetch) => {
+    filter.value[key] = +value;
 
-  // Set filter to browser path
+    // Set filter to browser path
 
-  if (notFetch !== true) {
-    const { leagues, discipline } = filter.value;
-    const path = { ...route.query };
+    if (notFetch !== true) {
+      const { leagues, discipline } = filter.value;
+      const path = { ...route.query };
 
-    if (leagues === 0) delete path.leagues;
-    else path.leagues = leagues;
+      if (leagues === 0) delete path.leagues;
+      else path.leagues = leagues;
 
-    if (discipline === 0) delete path.discipline;
-    else path.discipline = discipline;
+      if (discipline === 0) delete path.discipline;
+      else path.discipline = discipline;
 
-    window.history.replaceState(null, '', '?' + new URLSearchParams(path).toString());
+      window.history.replaceState(null, '', '?' + new URLSearchParams(path).toString());
 
-    fetchMethod('?' + new URLSearchParams(path).toString());
-  }
-};
+      fetchMethod('?' + new URLSearchParams(path).toString());
+    }
+  };
 
-onMounted(() => {
-  if (firstAction !== false) {
+  onMounted(() => {
     changeFilter('leagues', route.query.leagues || 0, true);
     changeFilter('discipline', route.query.discipline || 0, true);
 
-    fetchMethod('?' + new URLSearchParams(route.query).toString());
-  }
-});
+    if (firstAction !== false) {
+      fetchMethod('?' + new URLSearchParams(route.query).toString());
+    }
+  });
 </script>
 
 <style scoped>
-.flex {
-  display: flex;
-  flex-wrap: wrap;
-  margin-top: 3rem;
-  flex-wrap: wrap;
-  /* align-items: center; */
-  justify-content: center;
-  gap: 20px;
-}
-ul {
-  margin-top: 0;
-}
-button.active {
-  pointer-events: none;
-}
+  .flex {
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 3rem;
+    flex-wrap: wrap;
+    /* align-items: center; */
+    justify-content: center;
+    gap: 20px;
+  }
+  ul {
+    margin-top: 0;
+  }
+  button.active {
+    pointer-events: none;
+  }
 </style>
