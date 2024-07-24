1e067583e8f7dee88c5347343ee7f85542f095ea
jpaberzs <janis.paberzs18@gmail.com>
2024-07-24 10:54:40 +0300

    fix: added bg on match page

--- modules/tournaments/pages/[id]/schedule/[id].vue
+++ modules/tournaments/pages/[id]/schedule/[id].vue
@@ -4,6 +4,7 @@
       :title="dataMatch.title?.rendered"
       :breadcrumbArr="breadcrumb"
       class="sHeaderBlock sHeaderBlock--detailed sHeaderBlock--match"
+      bg="/img/schedule-bg.jpg"
     >
       <div class="header-item sTournamentOverview--match">
         <template v-if="isMobile">
@@ -74,69 +75,69 @@
 </template>
 
 <script setup lang="ts">
-  import type { commandInterface } from '@/modules/tournaments/interfaces/interface';
+import type { commandInterface } from '@/modules/tournaments/interfaces/interface';
 
-  import MatchHeader from '@/modules/tournaments/components/Match/MatchHeader.vue';
-  import PersonCard from '@/modules/tournaments/components/Match/PersonCard.vue';
-  import JoinGameModal from '@/modules/tournaments/components/Match/JoinGameModal.vue';
-  import Broadcast from '@/modules/tournaments/components/Match/Broadcast.vue';
-  import { useGlobalStore } from '~/store/globalStore';
-  import MatchModal from '~/modules/tournaments/components/Grid/MatchModal.vue';
-  const globalStore = useGlobalStore();
-  const { isReferee, isUserAuth } = storeToRefs(globalStore);
+import MatchHeader from '@/modules/tournaments/components/Match/MatchHeader.vue';
+import PersonCard from '@/modules/tournaments/components/Match/PersonCard.vue';
+import JoinGameModal from '@/modules/tournaments/components/Match/JoinGameModal.vue';
+import Broadcast from '@/modules/tournaments/components/Match/Broadcast.vue';
+import { useGlobalStore } from '~/store/globalStore';
+import MatchModal from '~/modules/tournaments/components/Grid/MatchModal.vue';
+const globalStore = useGlobalStore();
+const { isReferee, isUserAuth } = storeToRefs(globalStore);
 
-  const { id } = useRoute().params;
-  const tournamentID = useRoute().fullPath.split('/')[2];
+const { id } = useRoute().params;
+const tournamentID = useRoute().fullPath.split('/')[2];
 
-  import { useTournamentPageStore } from '@/modules/tournaments/store/TournamentPageStore';
-  const tournamentPageStore = useTournamentPageStore();
-  const { currentID, data } = storeToRefs(tournamentPageStore);
+import { useTournamentPageStore } from '@/modules/tournaments/store/TournamentPageStore';
+const tournamentPageStore = useTournamentPageStore();
+const { currentID, data } = storeToRefs(tournamentPageStore);
 
-  import { useMyMatchStore } from '~/modules/tournaments/store/MatchStore';
-  const matchStore = useMyMatchStore();
-  await matchStore.fetchData(id as string);
+import { useMyMatchStore } from '~/modules/tournaments/store/MatchStore';
+const matchStore = useMyMatchStore();
+await matchStore.fetchData(id as string);
 
-  const { dataMatch, broadcastIframe } = storeToRefs(matchStore);
+const { dataMatch, broadcastIframe } = storeToRefs(matchStore);
 
-  const isOpen = ref(true);
-  const toggle = () => {
-    isOpen.value = !isOpen.value;
-  };
+const isOpen = ref(true);
+const toggle = () => {
+  isOpen.value = !isOpen.value;
+};
 
-  const breakpoint = 991;
-  const isMobile = ref(window.innerWidth <= breakpoint);
-  const updateIsMobile = () => {
-    isMobile.value = window.innerWidth <= breakpoint;
-  };
+const breakpoint = 991;
+const isMobile = ref(window.innerWidth <= breakpoint);
+const updateIsMobile = () => {
+  isMobile.value = window.innerWidth <= breakpoint;
+};
 
-  import { useBreadcrumbsStore } from '~/store/BreadcrumbStore';
-  const breadcrumbsStore = useBreadcrumbsStore();
+import { useBreadcrumbsStore } from '~/store/BreadcrumbStore';
+const breadcrumbsStore = useBreadcrumbsStore();
 
-  // const isAccessRole = computed(() => {
-  //   if (globalStore.isUserAuth) {
-  //     if (globalStore.userData.roles.includes('administrator')) return true;
-  //     if (globalStore.userData.roles.includes('judge')) return true;
+// const isAccessRole = computed(() => {
+//   if (globalStore.isUserAuth) {
+//     if (globalStore.userData.roles.includes('administrator')) return true;
+//     if (globalStore.userData.roles.includes('judge')) return true;
 
-  //     return false;
-  //   }
-  //   return false;
-  // });
+//     return false;
+//   }
+//   return false;
+// });
 
+updateIsMobile();
+onMounted(() => {
   updateIsMobile();
-  onMounted(() => {
-    updateIsMobile();
-    window.addEventListener('resize', updateIsMobile);
+  window.addEventListener('resize', updateIsMobile);
 
-    if (tournamentID !== currentID.value || tournamentPageStore.currentID === '') {
-      tournamentPageStore.fetchData(tournamentID).then(() => {
-        // console.log(tournamentPageStore.data.title);
-        // matchStore.dataTournament = tournamentPageStore.data;
-        breadcrumbsStore.setNameFromIds(tournamentPageStore.data?.title);
-      });
-    }
-  });
-  onUnmounted(() => {
-    updateIsMobile();
-    window.removeEventListener('resize', updateIsMobile);
-  });
+  if (tournamentID !== currentID.value || tournamentPageStore.currentID === '') {
+    tournamentPageStore.fetchData(tournamentID).then(() => {
+      // console.log(tournamentPageStore.data.title);
+      // matchStore.dataTournament = tournamentPageStore.data;
+      breadcrumbsStore.setNameFromIds(tournamentPageStore.data?.title);
+    });
+  }
+});
+onUnmounted(() => {
+  updateIsMobile();
+  window.removeEventListener('resize', updateIsMobile);
+});
 </script>
