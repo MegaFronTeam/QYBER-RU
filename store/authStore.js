5eb6e88c21f781f76b6ba772104e5cd65cf2261a
PAVEL KHALUNIN <wol1414@gmail.com>
2024-09-09 16:30:46 +0200

    feat: add dots

--- store/authStore.js
+++ store/authStore.js
@@ -46,8 +46,8 @@ export const useAuthStore = defineStore('auth', () => {
       console.log('API_KEY', data[0]);
       console.log('email', dataFormLogin.value.email);
 
-      Cookies.set('API_KEY', data[0], { path: '/' });
-      Cookies.set('email', dataFormLogin.value.email, { path: '/' });
+      Cookies.set('API_KEY', data[0], { domain: '.qyber.ru', path: '/' });
+      Cookies.set('email', dataFormLogin.value.email, { domain: '.qyber.ru', path: '/' });
       userStore.getUserData();
       router.back();
     }
